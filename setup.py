eb5ac744f6474fb6affa99956ac53b15615127c7
anyi <anyi@mac.my>
2018-02-04 23:30:45 +0800

    add dashboard

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-vali',
-    version='0.1.0',
+    version='0.1.1',
     description=(
         'a django admin theme using vali-admin'
     ),
---
+++ vali/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from .views import DashboardView
+urlpatterns = [
+    url('^dashboard/', DashboardView.as_view())
+]
