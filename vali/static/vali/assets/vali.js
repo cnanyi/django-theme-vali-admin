bba2ad99029049199c117bf89bf7a0f2f611207e
anyi <anyi@mac.my>
2018-06-22 22:41:50 +0800

    auth.user permission , use CheckboxSelectMultiple widget and bootstrap's list-group style todo : group it by text

--- vali/static/vali/assets/vali.js
+++ vali/static/vali/assets/vali.js
@@ -9,5 +9,49 @@
     });
     if ($('.vali-multicheckbox').length > 0){
         $('.vali-multicheckbox li').addClass('list-group-item');
+        // ul
+        $('.vali-multicheckbox').each(function(){
+            // li
+            var inputs = {};
+            $(this).children().each(function(){
+                var lbl = $(this).find('label');
+                if (lbl.length > 0){
+                    var txts = lbl.text().trim().split('|');
+                    // app | model | permissions
+                    if (txts.length  == 3){
+                        var appname = txts[0].trim();
+                        var modelname = txts[1].trim();
+                        var permission = txts[2].trim();
+                        //lbl.text(permission);
+                        if (appname in inputs){
+                            if (modelname in inputs[appname]){
+                                inputs[appname][modelname].push(lbl);
+                            }else{
+                                //
+                            }
+                        }else{
+                            inputs[appname] = {};
+                            inputs[appname][modelname] = [lbl];
+                        }
+                    }
+                }
+            });
+
+            var html = "";
+            for (var key in inputs) {
+                html += '<li class="list-group-item"><div class="row"><span class="col-3">'+key+'</span>';
+                for (var mkey in inputs[key]){
+                    html += '<div class="col-9 row"><span class="col">'+mkey+'</span>';
+                    inputs[key][mkey].sort();
+                    for (var inputkey in inputs[key][mkey]){
+                        html += '<div class="col">'+ inputs[key][mkey][inputkey].prop('outerHTML')+'</div>';
+                    }
+                    html += '</div>';
+                }
+                html += '</div></li>';
+            }
+            $(this).children().remove();
+            $(this).html(html);
+        });
     }
 })();
