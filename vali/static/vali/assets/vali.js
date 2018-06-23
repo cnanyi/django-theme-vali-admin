bd65eafdf4cabf391d1806a82dbaed0635eebc23
anyi <anyi@mac.my>
2018-06-23 22:01:08 +0800

    make user permission three column

--- vali/static/vali/assets/vali.js
+++ vali/static/vali/assets/vali.js
@@ -8,10 +8,11 @@
         $('.object-tools').toggleClass('object-tools-mr');
     });
     if ($('.vali-multicheckbox').length > 0){
-        $('.vali-multicheckbox li').addClass('list-group-item');
+        //$('.vali-multicheckbox li').addClass('list-group-item');
         // ul
         $('.vali-multicheckbox').each(function(){
             // li
+            var is_perm_label = false;
             var inputs = {};
             $(this).children().each(function(){
                 var lbl = $(this).find('label');
@@ -19,39 +20,43 @@
                     var txts = lbl.text().trim().split('|');
                     // app | model | permissions
                     if (txts.length  == 3){
+                        is_perm_label = true;
                         var appname = txts[0].trim();
                         var modelname = txts[1].trim();
                         var permission = txts[2].trim();
                         //lbl.text(permission);
                         if (appname in inputs){
                             if (modelname in inputs[appname]){
-                                inputs[appname][modelname].push(lbl);
+                                inputs[appname][modelname].push(lbl.find('input').prop('outerHTML')+permission);
                             }else{
                                 //
                             }
                         }else{
                             inputs[appname] = {};
-                            inputs[appname][modelname] = [lbl];
+                            inputs[appname][modelname] = [lbl.find('input').prop('outerHTML')+permission];
                         }
                     }
                 }
             });
-
-            var html = "";
-            for (var key in inputs) {
-                html += '<li class="list-group-item"><div class="row"><span class="col-3">'+key+'</span>';
-                for (var mkey in inputs[key]){
-                    html += '<div class="col-9 row"><span class="col">'+mkey+'</span>';
-                    inputs[key][mkey].sort();
-                    for (var inputkey in inputs[key][mkey]){
-                        html += '<div class="col">'+ inputs[key][mkey][inputkey].prop('outerHTML')+'</div>';
+            if (is_perm_label){
+                var html = "";
+                for (var key in inputs) {
+                    html += '<li class="list-group-item"><div class="row"><span class="col-2">'+key+'</span>';
+                    for (var mkey in inputs[key]){
+                        html += '<div class="col row"><span class="col">'+mkey+'</span>';
+                        inputs[key][mkey].sort();
+                        for (var inputkey in inputs[key][mkey]){
+                            html += '<div class="col">'+ inputs[key][mkey][inputkey]+'</div>';
+                        }
+                        html += '</div>';
                     }
-                    html += '</div>';
+                    html += '</div></li>';
                 }
-                html += '</div></li>';
+                $(this).children().remove();
+                $(this).html(html);
+            }else{
+                $(this).children().addClass('list-group-item');
             }
-            $(this).children().remove();
-            $(this).html(html);
         });
     }
 })();
